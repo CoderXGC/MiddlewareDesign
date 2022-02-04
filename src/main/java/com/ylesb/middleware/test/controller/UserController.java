f033cceda9e31d089fea9b12ea3a665010039f5f
CoderXGC <xgc@tom.com>
2022-02-04 22:43:12 +0800

    自定义拦截方法

--- src/main/java/com/ylesb/middleware/test/controller/UserController.java
+++ src/main/java/com/ylesb/middleware/test/controller/UserController.java
@@ -8,6 +8,7 @@ package com.ylesb.middleware.test.controller;
  * @date 2022/2/119:17
  */
 import com.ylesb.middleware.hystrix.annotation.DoHystrix;
+import com.ylesb.middleware.methodext.annotation.DoMethodExt;
 import com.ylesb.middleware.whitelist.annotation.DoWhiteList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -53,6 +54,27 @@ public class UserController {
         logger.info("查询用户信息，userId：{}", userId);
        // Thread.sleep(1000);
         return new UserInfo("虫虫:" + userId, 19, "天津市东丽区万科赏溪苑14-0000");
+    }
+    /**
+     * 放行：http://localhost:8081/api/queryUserInfo?userId=aaa
+     * 拦截：http://localhost:8081/api/queryUserInfo?userId=bbb
+     */
+    @DoMethodExt(method = "blacklist", returnJson = "{\"code\":\"1111\",\"info\":\"自定义校验方法拦截，不允许访问！\"}")
+    @RequestMapping(path = "/api/queryUserInfo", method = RequestMethod.GET)
+    public UserInfo queryUserInfoMethodExt(@RequestParam String userId) {
+        logger.info("查询用户信息，userId：{}", userId);
+        return new UserInfo("虫虫:" + userId, 19, "天津市东丽区万科赏溪苑14-0000");
+    }
+
+    /**
+     * 自定义黑名单，拦截方法
+     */
+    public boolean blacklist(@RequestParam String userId) {
+        if ("bbb".equals(userId) || "222".equals(userId)) {
+            logger.info("拦截自定义黑名单用户 userId：{}", userId);
+            return false;
+        }
+        return true;
     }
 
 }
